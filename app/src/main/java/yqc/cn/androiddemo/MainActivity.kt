eeffdc29f5f3f29d5d5c5c5c0d81d5f0fa4ec9e7
杨旗超 <793477679@qq.com>
2018-04-20 11:21:05 +0800

    初始化

---
+++ app/src/main/java/yqc/cn/androiddemo/MainActivity.kt
@@ -0,0 +1,18 @@
+package yqc.cn.androiddemo
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val list:ArrayList<String>
+        val i=5
+
+    }
+}
